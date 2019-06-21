06419a9bcb8198d07e06ed0cd12361a5a68ffd1b
0anac0 <anaclaucf@hotmail.com>
2019-06-21 06:32:16 -0300

    Boa noite
    Não consegui botar animações, boa noite.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//move o indicador de onde está a sessão do menu. essa funcao é chamada quando o menu é clicado ou assim q o mouse ou teclado mexe em cada sessão
 function mover(sessao){
 var indicador = document.getElementById('menu-indicador');
 if(sessao==1){
@@ -10,4 +11,17 @@ if(sessao==1){
 	indicador.style.top = 64.3+'%';}
 
 
+}
+//tentativa de mudar a posicao das arvores a partir da posicao do mouse
+function animarArvores(id){
+	var arvores=document.getElementById(id);
+	console.log(arvores);
+var mouseX=getCursorX() ;
+ 	document.getElementById(id).style.left=-mouseX*document.getElementById(id).style.left;
+
+ 
+}
+function getCursorX(e) {
+	return ((window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft))
+ ; 
 }
