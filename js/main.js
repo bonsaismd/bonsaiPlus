1b04a1855009b4bbd5706245d1d0884d35a70e7b
0anac0 <anaclaucf@hotmail.com>
2019-06-21 04:53:58 -0300

    juntando sem animação

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function mover(sessao){
+var indicador = document.getElementById('menu-indicador');
+if(sessao==1){
+	indicador.style.top = 32.2+'%';}
+	if(sessao==2){
+	indicador.style.top = 43+'%';}
+	if(sessao==3){
+	indicador.style.top = 53.5+'%';}
+	if(sessao==4){
+	indicador.style.top = 64.3+'%';}
+
+
+}
